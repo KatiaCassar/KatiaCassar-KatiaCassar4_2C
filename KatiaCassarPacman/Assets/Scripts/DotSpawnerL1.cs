79e02bfe98c75a2a184a11ebf3c485a1be03e47c
Katia Cassar <42733677+KatiaCassar@users.noreply.github.com>
2019-11-27 21:21:55 +0100

    Task 2 a (Dots spawn however not in the right position)
    Task 2: Prepare the Game (SE2 – 10 marks)
    
    a. Prepare the dots to spawn automatically at runtime using scripts DotSpawnerL1.cs and DotSpawnerL2.cs (4 marks)

---
+++ KatiaCassarPacman/Assets/Scripts/DotSpawnerL1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotSpawnerL1 : MonoBehaviour
+{
+    GameObject[] dot;
+    GameObject randomDot;
+
+    Vector2 DotPosition, startingPosition;
+    //both of these variables are positions (x and y) so thier type is Vector 2
+
+
+    void Start()
+    {
+        startingPosition = transform.position;
+        LoadDotFromResources();
+        GetRandomDot();
+        SpawnDot();
+    }
+
+    void LoadDotFromResources()
+    {
+        dot = Resources.LoadAll<GameObject>("DotsL1");
+        /* The Resources class is responsible for functionality related to loading
+         * assets from the assets folder. To work we need to have a folder named
+         * Resources and all assets to be loaded need to be placed inside this
+         * folder. The type for prefabs is GameObject
+         */
+
+        DotPosition = transform.position;
+    }
+
+    void GetRandomDot()
+    {
+        int randomNumber = Random.Range(0, dot.Length); //random index from array
+
+        randomDot = dot[randomNumber];
+    }
+
+    void SpawnDot()
+    {
+        Instantiate(randomDot, DotPosition, transform.rotation);
+    }
+}
