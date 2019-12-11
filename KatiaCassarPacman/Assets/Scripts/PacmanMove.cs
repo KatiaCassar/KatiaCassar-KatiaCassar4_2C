f18ffba12ef8230e03eb8022f32550f3d3742869
Katia Cassar <42733677+KatiaCassar@users.noreply.github.com>
2019-12-11 17:11:24 +0100

    Pacman Moves

---
+++ KatiaCassarPacman/Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float padding = 0.5f;
+    float xMin, xMax, yMin, yMax;
+
+    void Start()
+    {
+        SetUpMoveBountaries();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    { 
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax); 
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector3(newXPos, newYPos, transform.position.z);
+    }
+
+    void SetUpMoveBountaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
