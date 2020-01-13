272fb38e8d03f0e1057218f250ed5143ff62a9c3
Katia Cassar <42733677+KatiaCassar@users.noreply.github.com>
2020-01-13 14:37:44 +0100

    Practical 1 Part 2

---
+++ 42C-KatiaCassar-Billiard/Assets/Scripts/ColouredBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColouredBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D col){
+        DestroyBallWhenHitsHole(col, gameObject);
+    }
+    
+    public void DestroyBallWhenHitsHole(Collision2D collision, GameObject ball){
+
+        if(gameObject.tag == "hole")
+        {
+            if(ball.tag == "ColouredBall"){
+                Destroy(gameObject);
+            }
+            
+            else if (ball.tag == "WhiteBall"){
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ 42C-KatiaCassar-Billiard/Assets/Scripts/WhiteBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteBall : MonoBehaviour
+{
+    int ranX;
+    int ranY;
+    
+    ColouredBall colouredball;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        colouredball = FindObjectOfType<ColouredBall>();
+        ranX = Random.Range(-1, 2);
+        ranY = Random.Range(15, 21);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //left click pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            //shoot the Ball
+            colouredball.GetComponent<Rigidbody2D>().velocity = new Vector2(ranX, ranY);
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D col){
+        colouredball.DestroyBallWhenHitsHole(col, gameObject);
+    }
+    
+}
